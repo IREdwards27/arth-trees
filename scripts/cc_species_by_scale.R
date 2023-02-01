b94818fcfd293242a5fe41bb2aadc4ee18ca4d9a
Indigo <iredward27@gmail.com>
2023-02-01 17:58:10 -0500

    assessed species branch numbers by scale

---
+++ scripts/cc_species_by_scale.R
@@ -0,0 +1,71 @@
+library(tidyverse)
+
+cc_raw <- read_csv('data/CC_fullDataset_2022-11-30.csv')
+
+plant_species <- read_csv('data/plantNames_2023-02-01.csv')
+
+names(cc_raw)
+
+local_stats <- cc_raw %>% 
+  select(ID, LocalDate, julianday, Year, ObservationMethod, WetLeaves, PlantSpecies, NumberOfLeaves, SiteFK, Name, Latitude, Longitude, Region, PlantFK) %>% 
+  unique() %>% 
+  filter(
+    ObservationMethod == 'Visual',
+    Year %in% c(2017:2019,2021:2022),
+    julianday >= 150,
+    julianday <= 192,
+    between(Latitude, 35.5, 36.5),
+    between(Longitude, -79.5, -78.5)) %>% 
+  left_join(
+    plant_species %>% 
+      select(cleanedName, sciName),
+    by = c('PlantSpecies' = 'cleanedName')) %>% 
+  group_by(sciName) %>% 
+  summarize(
+    n_branches = length(unique(PlantFK)),
+    n_surveys = length(unique(ID)))
+
+local_stats %>% 
+  filter(n_branches > 10, n_surveys > 100)
+
+NC_stats <- cc_raw %>% 
+  select(ID, LocalDate, julianday, Year, ObservationMethod, WetLeaves, PlantSpecies, NumberOfLeaves, SiteFK, Name, Latitude, Longitude, Region, PlantFK) %>% 
+  unique() %>% 
+  filter(
+    ObservationMethod == 'Visual',
+    Year %in% c(2017:2019,2021:2022),
+    julianday >= 150,
+    julianday <= 192,
+    Region == 'NC') %>% 
+  left_join(
+    plant_species %>% 
+      select(cleanedName, sciName),
+    by = c('PlantSpecies' = 'cleanedName')) %>% 
+  group_by(sciName) %>% 
+  summarize(
+    n_branches = length(unique(PlantFK)),
+    n_surveys = length(unique(ID)))
+
+NC_stats %>% 
+  filter(n_branches > 10, n_surveys > 100)
+
+fivestate_stats <- cc_raw %>% 
+  select(ID, LocalDate, julianday, Year, ObservationMethod, WetLeaves, PlantSpecies, NumberOfLeaves, SiteFK, Name, Latitude, Longitude, Region, PlantFK) %>% 
+  unique() %>% 
+  filter(
+    ObservationMethod == 'Visual',
+    Year %in% c(2017:2019,2021:2022),
+    julianday >= 150,
+    julianday <= 192,
+    Region %in% c('NC','VA','GA','SC','TN')) %>% 
+  left_join(
+    plant_species %>% 
+      select(cleanedName, sciName),
+    by = c('PlantSpecies' = 'cleanedName')) %>% 
+  group_by(sciName) %>% 
+  summarize(
+    n_branches = length(unique(PlantFK)),
+    n_surveys = length(unique(ID)))
+
+fivestate_stats %>% 
+  filter(n_branches > 10, n_surveys > 100)
